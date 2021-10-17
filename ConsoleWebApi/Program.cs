21d62c63b1770f95ca37b4dd555de64f69ca0395
Andrei Ladonin <ladonin2001@outlook.com>
2021-10-17 20:41:19 +0400

    addedPost

--- ConsoleWebApi/Program.cs
+++ ConsoleWebApi/Program.cs
@@ -4,6 +4,8 @@ using System;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace ConsoleWebApi
 {
@@ -11,10 +13,37 @@ namespace ConsoleWebApi
     {
         static void Main(string[] args)
         {
-            WebRequest myRequest = WebRequest.Create("https://localhost:44361/api/Categories");
 
+
+            while (true)
+            {
+                Console.WriteLine("Получить Или Добавить get/add");
+
+                string answer = Console.ReadLine();
+
+                if (answer == "get")
+                {
+                    Get();
+                }
+                else if (answer == "add")
+                {
+                    Console.WriteLine("Введите имя категории");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("Введите описании категории");
+                    string desc = Console.ReadLine();
+
+                    Post(name, desc);
+
+                }
+            }
             
-            WebResponse myResponse = myRequest.GetResponse();
+        }
+
+        public static void Get ()
+        {
+            WebRequest req = WebRequest.Create("https://localhost:44361/api/categories");
+            WebResponse myResponse = req.GetResponse();
 
             string response;
 
@@ -32,11 +61,41 @@ namespace ConsoleWebApi
             JArray jArray = (JArray)json;
 
 
-            Console.WriteLine("|\tName\t|\tDescription");
+            Console.WriteLine("ID\t|\tName\t|\tDescription");
             foreach (var items in jArray)
             {
-                Console.WriteLine($"|{items["name"]}\t|{items["desc"]}");
+                Console.WriteLine($"|{items["id"]} \t |{items["name"]}\t|{items["desc"]}");
             }
+        }
+
+        public static void Post (string name, string desc)
+        {
+            JObject json = new JObject();
+
+            json["CategoryName"] = name;
+            json["Description"] = desc;
+
+
+            WebRequest req = WebRequest.Create("https://localhost:44361/api/categories");
+            req.Method = "POST";
+
+            req.ContentType = "application/json";
+
+            using (var streamWriter = new StreamWriter(req.GetRequestStream()))
+            {
+
+                streamWriter.Write(json.ToString());
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)req.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+
         }
     }
 }
